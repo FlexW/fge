165e73fbaff8a53d47f40fcd3107dcb30ba299a2
Felix Weilbach <felix.weilbach@t-online.de>
2021-01-16 15:33:48 +0100

    Cleanup

--- src/engine/graphic/imgui.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// clang-format off
-#include <imgui.h>
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include <imgui_impl_opengl3.h>
-#include <imgui_impl_glfw.h>
-// clang-format on
--- src/engine/platform/bullet/bullet.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <btBulletDynamicsCommon.h>
